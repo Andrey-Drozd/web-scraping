fdaeaa399a28586c91ae9e4ff9ef35b2bdbd2149
Andrey Drozd <bestspacemail@gmail.com>
2023-02-01 14:50:56 +0300

    add new start url for parsing all ads

--- src/realt.by/constants/urls.ts
+++ src/realt.by/constants/urls.ts
@@ -1,6 +1,6 @@
 export const START_PAGE = 0
 
 export const START_URL =
-  'https://realt.by/rent/flat-for-long/?search=eJxdy8EKgCAQBNCv0bNadusS%2FccSsoRQuawKfX5aEeRp4M0Mh7BHYScUdi6hSoxG8l911a5VU7VvtatqW%2B2rDpLYuyLTivdXqU%2B2R4yVJ7iQjwTkXcqM8ZtrCRFTpgKMDggZaHkrdQHLkTnz&view=1'
+  'https://realt.by/rent/flat-for-long/?search=eJyLL04tKS1QNXUqSk2OL0gtii9ITE9VNXWxNTUAAIv1CUk%3D'
 
 export const START_PREPARED_URL = `${START_URL}&page=${START_PAGE}`
